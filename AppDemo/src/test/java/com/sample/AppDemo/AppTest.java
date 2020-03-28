478674db085af60a837ffc6b0ee1941b247065b1
StarGitMax <Sri.Tirunaghari@prolifics.com>
2020-03-28 15:19:47 +0530

    modified

--- AppDemo/src/test/java/com/sample/AppDemo/AppTest.java
+++ AppDemo/src/test/java/com/sample/AppDemo/AppTest.java
@@ -7,9 +7,13 @@ import org.testng.annotations.Test;
  */
 public class AppTest
 {
+
+	 String pwd = System.getProperty("browser");
+	
    @Test
     public void testApp()
     {
-        System.out.println("This is for Testing Ouput in  Concource Console Using Maven");
+        System.out.println("This is for Testing Ouput in Concource Console Using Maven");
+        System.out.println("Project Working Dirctory: "+pwd);
     }
 }
