a0f6e5b44818bd5c675a8cf868ec8ced234cb065
StarGitMax <Sri.Tirunaghari@prolifics.com>
2020-03-28 11:23:17 +0530

    modified sh file

--- test-script.sh
+++ test-script.sh
@@ -8,3 +8,7 @@ cd $_ROOT/$1/AppDemo/
 mvn test
 _ROOT1=$(pwd)
 echo "ending test-script from directory: $_ROOT1"
+cp -R $_ROOT1/target/* mvn-output/
+echo "Hi, I'm sleeping for 2 minutes..."
+sleep 2m
+echo "all Done."
