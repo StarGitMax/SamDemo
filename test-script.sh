862fdf7bfb2b2cbc21fddb76e12d5de22c79cb5e
StarGitMax <Sri.Tirunaghari@prolifics.com>
2020-03-28 15:21:59 +0530

    modified sh file

--- test-script.sh
+++ test-script.sh
@@ -5,7 +5,7 @@ _ROOT=$(pwd)
 echo "starting test-script from directory: $_ROOT"
 echo "Input Argument: $1"
 cd $_ROOT/$1/AppDemo/
-mvn test
+mvn test -Dpwd=$_ROOT
 _ROOT1=$(pwd)
 echo "ending test-script from directory: $_ROOT1"
 echo "Source directory: $_ROOT1/target/"
