ee18d8db5ff209371030fb198aea1614b5280cd8
StarGitMax <Sri.Tirunaghari@prolifics.com>
2020-04-05 10:04:49 +0530

    modified sh file

--- test-script.sh
+++ test-script.sh
@@ -5,6 +5,20 @@ mvn -v
 _ROOT=$(pwd)
 echo "starting test-script from directory: $_ROOT"
 echo "Input Argument: $1"
+M2_HOME=${HOME}/.m2
+mkdir -p ${M2_HOME}
+M2_LOCAL_REPO="${_ROOT}/.m2"
+mkdir -p "${M2_LOCAL_REPO}/repository"
+cat > ${M2_HOME}/settings.xml <<EOF
+ 
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
+</settings>
+ 
+EOF
 cd $_ROOT/$1/AppDemo/
 mvn test -Dpwd=$_ROOT
 _ROOT1=$(pwd)
