121835bc8d911a5a7b5c2ae610a3016bdb881784
StarGitMax <Sri.Tirunaghari@prolifics.com>
2020-04-13 14:57:29 +0530

    added selenium dependencies and selenium Test code

--- AppDemo/src/test/java/com/sample/AppDemo/AppTest.java
+++ AppDemo/src/test/java/com/sample/AppDemo/AppTest.java
@@ -1,19 +1,69 @@
 package com.sample.AppDemo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
-{
-
+{ 
+	
+	 public static WebDriver driver;
 	 String pwd = System.getProperty("pwd");
+	 String sPath = System.getProperty("user.dir");
+	 static String OS = System.getProperty("os.name").toLowerCase();
 	
    @Test
-    public void testApp()
+    public void testApp() throws Exception
     {
-        System.out.println("This is for Testing Ouput in Concource Console Using Maven");
-        System.out.println("Project Working Dirctory: "+pwd);
+	   System.out.println("Osname "+OS);
+	   System.out.println("This is for Testing Ouput in Concource Console Using Maven");
+       System.out.println("Project Working Dirctory: "+pwd);
+	   if (isWindows()) {
+			System.out.println("This is Windows");
+			  System.setProperty("webdriver.chrome.driver", sPath+"\\Drivers\\chromedriver.exe");
+		} else if (isMac()) {
+			System.out.println("This is Mac");
+		} else if (isUnix()) {
+			System.out.println("This is Unix or Linux");
+			  System.setProperty("webdriver.chrome.driver", sPath+"\\Drivers\\chromedriver");
+		} else if (isSolaris()) {
+			System.out.println("This is Solaris");
+		} else {
+			System.out.println("Your OS is not support!!");
+		}
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(10000);
+		System.out.println("Title "+driver.getTitle());
+         driver.close();
+         driver.quit();
     }
+   
+   
+   public static boolean isWindows() {
+ 
+		return (OS.indexOf("win") >= 0);
+
+	}
+
+	public static boolean isMac() {
+
+		return (OS.indexOf("mac") >= 0);
+
+	}
+
+	public static boolean isUnix() {
+
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+		
+	}
+
+	public static boolean isSolaris() {
+
+		return (OS.indexOf("sunos") >= 0);
+
+	}
 }
