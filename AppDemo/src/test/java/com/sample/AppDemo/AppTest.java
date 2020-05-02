168da129b618ba871e1c2af91fd1ce4c129523b4
StarGitMax <Sri.Tirunaghari@prolifics.com>
2020-05-02 16:35:29 +0530

    Modified code

--- AppDemo/src/test/java/com/sample/AppDemo/AppTest.java
+++ AppDemo/src/test/java/com/sample/AppDemo/AppTest.java
@@ -1,5 +1,7 @@
 package com.sample.AppDemo;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -24,12 +26,14 @@ public class AppTest
        System.out.println("Project Working Dirctory: "+pwd);
 	   if (isWindows()) {
 			System.out.println("This is Windows");
-			  System.setProperty("webdriver.chrome.driver", sPath+"\\Drivers\\chromedriver.exe");
+			  //System.setProperty("webdriver.chrome.driver", sPath+"\\Drivers\\chromedriver.exe");
+			  System.setProperty("webdriver.chrome.driver", ClassLoader.getSystemResource("Drivers/chromedriver.exe").getPath());
 		} else if (isMac()) {
 			System.out.println("This is Mac");
 		} else if (isUnix()) {
 			System.out.println("This is Unix or Linux");
-			  System.setProperty("webdriver.chrome.driver", sPath+"//Drivers//chromedriver");
+			  //System.setProperty("webdriver.chrome.driver", sPath+"//Drivers//chromedriver");
+			 System.setProperty("webdriver.chrome.driver", ClassLoader.getSystemResource("Drivers/chromedriver").getPath());
 		} else if (isSolaris()) {
 			System.out.println("This is Solaris");
 		} else {
